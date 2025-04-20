dd690be2375b47aefe8c0bc3f2b4e067576568f9
CodingWithMK <032180115@ogr.uludag.edu.tr>
2025-04-21 00:25:31 +0300

    Password Generator built using Iterables & Iterators - Micro Project.

--- CHAPTER_32-Iterables_and_Iterators/Practicing_Projects/password_generator.py
+++ CHAPTER_32-Iterables_and_Iterators/Practicing_Projects/password_generator.py
@@ -1,8 +1,22 @@
 # ========== Password Generator using Iterables and Iterators ==========
 
 import random
+import string
 
 class PasswordGenerator:
-    def __init__(self, chars, length):
-        self.chars = chars
+    def __init__(self, length):
         self.length = length
+        self.chars = string.ascii_letters + string.digits + string.punctuation
+        self.generated = 0
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.generated >= self.length:
+            raise StopIteration
+        self.generated += 1
+        return random.choice(self.chars)
+
+if __name__ == "__main__":
+    print("Generated Password: ", "".join(PasswordGenerator(12)))
